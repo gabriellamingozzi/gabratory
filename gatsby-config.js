11416c7c1582611b5802bb10eac6e059c6439d2f
gabriellamingozzi <gabriellamingozzi@gmail.com>
2020-07-02 17:23:48 -0400

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `The Gabratory`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Gabriella Mingozzi`,
+      summary: `:)`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `My blog!`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `gabriellamingoz`,
     },
   },
   plugins: [
